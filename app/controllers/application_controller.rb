6261bdef496836f99ed214d128cb3fc693cde58e
Kazuki <i.kazu861@gmail.com>
2020-03-13 21:16:15 +0900

    [update]ヘッダーの変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,29 +1,26 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_current_user
+   
+  def after_sign_in_path_for(_resource)
+    current_user
+  end
+
+  def after_sign_out_path_for(_resource)
+    root_path
+  end
+
+  def after_sign_up_path_for(_resource)
+    current_user
+  end
+
+  def after_inactive_sign_up_path_for(_resource)
+    current_user
+  end
 
   protected
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
-  
-  def set_current_user
-    @current_user = User.find_by(id: session[:user_id])
-  end
-  
-  def authenticate_user
-    if @current_user == nil
-      flash[:notice] = "ログインが必要です"
-      redirect_to("/login")
-    end
-  end
-  
-  def forbid_login_user
-    if @current_user
-      flash[:notice] = "すでにログインしています"
-      redirect_to("/posts/index")
-    end
-  end
-
 end
