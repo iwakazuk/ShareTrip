02bf091efb6413d17730e6f7f8f67ac3628c3d2d
Kazuki <i.kazu861@gmail.com>
2020-03-14 21:05:52 +0900

    [fix]post_controllerをヘルパ-メソッドで書き換え

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,63 +1,68 @@
 class PostsController < ApplicationController
-    before_action :authenticate_user
-    before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
-    
+    before_action :authenticate_user!
+    before_action :correct_user, only: [:destroy]
+
     def index
       @posts = Post.all.order(created_at: :desc)
     end
     
     def show
-      @post = Post.find_by(id: params[:id])
+      @post = Post.find(params[:id])
       @user = @post.user
       @likes_count = Like.where(post_id: @post.id).count
     end
     
     def new
-      @post = Post.new
+      @post = current_user.new
     end
     
     def create
-      @post = Post.new(
-        content: params[:content],
-        user_id: @current_user.id
-      )
+      @post = Post.new(post_params)
+      @post.user_id = current_user.id
       if @post.save
         flash[:notice] = "投稿を作成しました"
-        redirect_to("/posts/index")
+        redirect_to current_user
       else
-        render("posts/new")
+        render :new
       end
     end
     
     def edit
-      @post = Post.find_by(id: params[:id])
+      @post = Post.find(params[:id])
     end
     
     def update
-      @post = Post.find_by(id: params[:id])
+      @post = Post.find(params[:id])
       @post.content = params[:content]
+      @post.user_id = current_user.id
       if @post.save
         flash[:notice] = "投稿を編集しました"
         redirect_to("/posts/index")
       else
-        render("posts/edit")
+        render :edit
       end
     end
     
     def destroy
-      @post = Post.find_by(id: params[:id])
+      @post = Post.find(params[:id])
       @post.destroy
       flash[:notice] = "投稿を削除しました"
       redirect_to("/posts/index")
     end
     
-    def ensure_correct_user
-      @post = Post.find_by(id: params[:id])
-      if @post.user_id != @current_user.id
-        flash[:notice] = "権限がありません"
-        redirect_to("/posts/index")
-      end
+    private
+
+    def posts_params
+      params.require(:post).permit(
+      :content,
+      :image
+      )
     end
     
+    def correct_user
+      @post = Post.find(params[:id])
+      redirect_to(root_url) unless @post.user == current_user
+    end
+
   end
   
