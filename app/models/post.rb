ddd8bbfb27c1a3a04723325daa71f87d07b38e92
Kazuki <i.kazu861@gmail.com>
2020-03-15 21:39:44 +0900

    [fix]likeを投稿削除に関連づけ

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,10 @@ class Post < ApplicationRecord
   validates :content, length: {maximum: 140}
   validates :user_id, presence: true
   mount_uploaders :image, ImageUploader
-  
+
+  has_many   :likes, dependent: :destroy
+  has_many   :liked_users, through: :likes, source: :user
+
   def user
     return User.find_by(id: self.user_id)
   end
