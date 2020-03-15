14fbd8f90d17de1808db7e0b28767a9aef6c39f0
Kazuki <i.kazu861@gmail.com>
2020-03-15 21:41:43 +0900

    [fix]不要コード削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, {presence: true}
-  validates :email, {presence: true, uniqueness: true}
-  validates :password, {presence: true}
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
   
   def posts
     return Post.where(user_id: self.id)
