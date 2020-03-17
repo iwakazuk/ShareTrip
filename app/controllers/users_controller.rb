c26d9d444060d29e230f69b59ea956678d2e69f9
Kazuki <i.kazu861@gmail.com>
2020-03-17 20:27:59 +0900

    [fix]デフォルトのlocaleを日本語にする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,19 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts.page(params[:page]).per(24)
+    @likes = @user.like_posts.page(params[:page]).per(24)
   end
   
   def likes
     @user = User.find_by(id: params[:id])
     @likes = Like.where(user_id: @user.id)
   end
+
+  private
+
+  def serch
+    @q = User.ransack(params[:q])
+  end
+  
 end
