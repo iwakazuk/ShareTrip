ea73bd3401228108dd337d1a12a4a58bad1d1889
Kazuki <i.kazu861@gmail.com>
2020-03-17 20:34:09 +0900

    [add]postテーブルにタイトルカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_14_101106) do
+ActiveRecord::Schema.define(version: 2020_03_15_152916) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_03_14_101106) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.string "image"
+    t.string "title"
   end
 
   create_table "users", force: :cascade do |t|
