c7af1304aabd0439fa1b14b3fac091758f0d907f
Kazuki <i.kazu861@gmail.com>
2020-03-21 20:28:48 +0900

    [fix]プロフィール画像をcarrierwaveで更新可能に変更

--- db/migrate/20200317165056_create_comments.rb
+++ db/migrate/20200317165056_create_comments.rb
@@ -1,10 +1,9 @@
-class CreateLikes < ActiveRecord::Migration[6.0]
+class CreateComments < ActiveRecord::Migration[6.0]
   def change
-    create_table :likes do |t|
+    create_table :comments do |t|
       t.string :content
       t.references :user, foreign_key: true
       t.references :post, foreign_key: true
-
 
       t.timestamps
     end
