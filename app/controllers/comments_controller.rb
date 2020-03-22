494b69acc8300127f4cf6cd6e0ab59ba8839e05c
Kazuki <i.kazu861@gmail.com>
2020-03-22 20:14:03 +0900

    [fix]インデント修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,33 +1,33 @@
 class CommentsController < ApplicationController
-    before_action :authenticate_user!
-    def create
-      @comment = current_user.comments.create(content: params[:comment][:content], post_id: params[:post_id])
-      @post = Post.find(params[:post_id])
-    end
-  
-    def destroy
-      @comment = Comment.find_by(post_id: params[:post_id], id: params[:id])
-      @comment_id = @comment.id
-      @comment.delete
-      @post = Post.find(params[:post_id])
-    end
-  
-    def edit
-      @post = Post.find(params[:post_id])
-      @comment = Comment.find_by(post_id: @post.id, id: params[:id])
-    end
-  
-    def update
-      @comment = Comment.find(params[:id])
-      @succeed = false
-      flash.now[:success] = "コメントを更新しました。"
-      @succeed = true if @comment.update(content: params[:comment][:content])
-    end
-  
-    private
-  
-    def comment_params
-      params.require(:comment, :post_id).permit(:content)
-    end
+  before_action :authenticate_user!
+  def create
+    @comment = current_user.comments.create(content: params[:comment][:content], post_id: params[:post_id])
+    @post = Post.find(params[:post_id])
   end
+
+  def destroy
+    @comment = Comment.find_by(post_id: params[:post_id], id: params[:id])
+    @comment_id = @comment.id
+    @comment.delete
+    @post = Post.find(params[:post_id])
+  end
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find_by(post_id: @post.id, id: params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @succeed = false
+    flash.now[:success] = "コメントを更新しました。"
+    @succeed = true if @comment.update(content: params[:comment][:content])
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment, :post_id).permit(:content)
+  end
+end
   
