9bf87b634a240bb3e1c9707195a79ab21b5ffeb2
Kazuki <i.kazu861@gmail.com>
2020-03-22 20:02:10 +0900

    [add]relationshipコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
-  get "/" => "home#top"
-  get "about" => "home#about"
   devise_for :users, controllers: {
     sessions:      'users/sessions',
     registrations: 'users/registrations'
   }
+
+  get "/" => "home#top"
+  get "about" => "home#about"
  
-  resources :users, only: %i[index show] do
-    resources :likes, only: %i[index]
+  resources :users, only: [:index, :show] do
+    resources :likes, only: [:index]
   end
 
   resources :posts do
-    resources :likes, only: %i[create destroy]
-    resources :comments, only: %i[create edit update destroy]
+    resources :likes, only: [:create, :destroy]
+    resources :comments, only: [:create, :edit, :update, :destroy]
   end
+
+  resources :relationships, only: [:create, :destroy]
 
 end
