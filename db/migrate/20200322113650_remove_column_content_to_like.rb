2a2aaa68cb9feaafde3f397b04dae1a235c0d027
Kazuki <i.kazu861@gmail.com>
2020-04-15 01:10:01 +0900

    [fix]DBファイル整理

--- db/migrate/20200322113650_remove_column_content_to_like.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnContentToLike < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :likes, :content, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_22_113650) do
+ActiveRecord::Schema.define(version: 2020_03_22_112109) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -33,11 +33,12 @@ ActiveRecord::Schema.define(version: 2020_03_22_113650) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
-    t.integer "user_id"
+    t.string "image"
+    t.string "title"
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "images"
-    t.string "title"
+    t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
